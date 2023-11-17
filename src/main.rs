e53bd54d593186e986265080463a9d32bc0d684a
Eirik <eirik.bjoroen@geodata.no>
2023-11-17 13:39:52 +0100

    Now collects content of section

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::io::Read;
-
 /// ## Sections
 /// ### Each section consists of:
 /// - one-byte section id,
@@ -7,25 +5,25 @@ use std::io::Read;
 /// - the actual contents, whose structure is dependent on the section id.
 ///
 /// Every section is optional; an omitted section is equivalent to the section being present with empty contents.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum WasmSection {
-    Custom,
-    Type,
-    Import,
-    Function,
-    Table,
-    Memory,
-    Global,
-    Export,
-    Start,
-    Element,
-    Code,
-    Data,
-    DataCount,
+    Custom(Vec<u8>),
+    Type(Vec<u8>),
+    Import(Vec<u8>),
+    Function(Vec<u8>),
+    Table(Vec<u8>),
+    Memory(Vec<u8>),
+    Global(Vec<u8>),
+    Export(Vec<u8>),
+    Start(Vec<u8>),
+    Element(Vec<u8>),
+    Code(Vec<u8>),
+    Data(Vec<u8>),
+    DataCount(Vec<u8>),
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let filename = String::from("./main.wasm");
+    let filename = String::from("./test.wasm");
     let f = std::fs::read(&filename)?;
 
     match parse_wasm_section(f) {
@@ -37,33 +35,32 @@ fn main() -> Result<(), std::io::Error> {
 }
 
 fn parse_wasm_section(f: Vec<u8>) -> Result<Vec<WasmSection>, std::io::Error> {
-    let mut bytes = f.bytes().skip(8);
+    let mut cursor = 8;
     let mut sections: Vec<WasmSection> = vec![];
 
-    while let Some(courser) = bytes.next() {
-        let section = match courser? {
-            0 => WasmSection::Custom,
-            1 => WasmSection::Type,
-            2 => WasmSection::Import,
-            3 => WasmSection::Function,
-            4 => WasmSection::Table,
-            5 => WasmSection::Memory,
-            6 => WasmSection::Global,
-            7 => WasmSection::Export,
-            8 => WasmSection::Start,
-            9 => WasmSection::Element,
-            10 => WasmSection::Code,
-            11 => WasmSection::Data,
-            12 => WasmSection::DataCount,
+    while cursor < f.len() {
+        let size_of_section = f[cursor + 1] as usize;
+
+        let section = match f[cursor] as u32 {
+            0 => WasmSection::Custom(f[cursor + 2..cursor + size_of_section + 2].to_vec()),
+            1 => WasmSection::Type(f[cursor + 2..cursor + size_of_section + 2].to_vec()),
+            2 => WasmSection::Import(f[cursor + 2..cursor + size_of_section + 2].to_vec()),
+            3 => WasmSection::Function(f[cursor + 2..cursor + size_of_section + 2].to_vec()),
+            4 => WasmSection::Table(f[cursor + 2..cursor + size_of_section + 2].to_vec()),
+            5 => WasmSection::Memory(f[cursor + 2..cursor + size_of_section + 2].to_vec()),
+            6 => WasmSection::Global(f[cursor + 2..cursor + size_of_section + 2].to_vec()),
+            7 => WasmSection::Export(f[cursor + 2..cursor + size_of_section + 2].to_vec()),
+            8 => WasmSection::Start(f[cursor + 2..cursor + size_of_section + 2].to_vec()),
+            9 => WasmSection::Element(f[cursor + 2..cursor + size_of_section + 2].to_vec()),
+            10 => WasmSection::Code(f[cursor + 2..cursor + size_of_section + 2].to_vec()),
+            11 => WasmSection::Data(f[cursor + 2..cursor + size_of_section + 2].to_vec()),
+            12 => WasmSection::DataCount(f[cursor + 2..cursor + size_of_section + 2].to_vec()),
             s => panic!("Unknown section id, {:?}", s),
         };
 
-        let number_to_skip = bytes.next().unwrap().unwrap();
-
-        bytes.nth(usize::try_from(number_to_skip).unwrap() - 1);
+        cursor = cursor + size_of_section + 2;
 
         sections.push(section);
-        println!("Section: {:#?}", sections);
     }
 
     Ok(sections)
@@ -75,9 +72,17 @@ mod tests {
 
     #[test]
     fn parses_section() {
-        let wasm: Vec<u8> = vec![00, 00, 00, 00, 00, 00, 00, 00, 01, 05, 00, 00, 00, 00, 00];
+        let wasm: Vec<u8> = vec![
+            00, 00, 00, 00, 00, 00, 00, 00, 01, 05, 00, 00, 00, 00, 00, 03, 02, 01, 0x7f,
+        ];
         let result = parse_wasm_section(wasm).unwrap();
 
-        assert_eq!(result, vec![WasmSection::Type])
+        assert_eq!(
+            result,
+            vec![
+                WasmSection::Type(vec![00, 00, 00, 00, 00]),
+                WasmSection::Function(vec![01, 127])
+            ]
+        )
     }
 }
